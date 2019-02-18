c3f4af7e1f4e5abd38245d5031908bc5d3002374
Owen <ophunt2000@gmail.com>
2019-02-18 16:16:02 -0600

    Update 1: Better Draws

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const canvas = document.getElementById("canvas")
-const ctx = canvas.getContext("2d")
-CANVAS_SIZE = 500
-SQUARES = 25
-SQUARE_SIZE = CANVAS_SIZE / SQUARES
-SPEED = 100
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+CANVAS_SIZE = 500;
+SQUARES = 25;
+SQUARE_SIZE = CANVAS_SIZE / SQUARES;
+SPEED = 100;
 
-canvas.height = CANVAS_SIZE
-canvas.width = CANVAS_SIZE
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
 
-running = false
+running = false;
 
-const resultP = document.getElementById("result")
+const resultP = document.getElementById("result");
 
 class Tron {
 	constructor(color, init_x, init_y, dir) {
@@ -18,46 +18,67 @@ class Tron {
 		this.x = init_x;
 		this.y = init_y;
 		this.dir = dir;
+		this.nextX = this.x;
+		this.nextY = this.y;
 		this.nextDir = dir;
 		this.lost = false;
-		occupiedSquares.add(this.x + (SQUARES + 10) * this.y)
-		this.drawToCanvas()
+		squareOccupations[this.x][this.y] += 1;
+		this.drawToCanvas();
 	};
 
 	drawToCanvas() {
-		drawSquare(this.color, this.x, this.y)
+		if (this.lost) {
+			this.color = "black";
+		}
+		drawSquare(this.color, this.x, this.y);
+	}
+
+	preMakeMove() {
+		switch(this.dir) {
+			case ("u"):
+				this.nextX -= 1;
+				break;
+			case("d"):
+				this.nextX += 1;
+				break;
+			case("l"):
+				this.nextY -= 1;
+				break;
+			case("r"):
+				this.nextY += 1;
+				break;
+			default:
+				throw "Illegal Direction!";
+		}
 	}
 
 	makeMove() {
-		switch(this.dir) {
-			case ("u"):
-				this.x -= 1;
-				break;
-			case("d"):
-				this.x += 1;
-				break;
-			case("l"):
-				this.y -= 1;
-				break;
-			case("r"):
-				this.y += 1;
-				break;
-			default:
-				throw "Illegal Direction!"
-		}
+		this.x = this.nextX;
+		this.y = this.nextY;
 	}
 
 	checkCollision() {
-		return occupiedSquares.has(this.x + (SQUARES + 10) * this.y) || outBounds(this.x, 0, SQUARES - 1) || outBounds(this.y, 0, SQUARES - 1)
+		return outBounds(this.x, 0, SQUARES - 1)
+				|| outBounds(this.y, 0, SQUARES - 1)
+				|| squareOccupations[this.x][this.y] > 1;
+	}
+
+	preMove() {
+		this.dir = this.nextDir;
+		this.preMakeMove();
+		try {
+			squareOccupations[this.nextX][this.nextY] += 1;
+		} catch (error) {
+			;
+		}
 	}
 
 	move() {
-		this.dir = this.nextDir
-		this.makeMove()
+		this.makeMove();
 		if(this.checkCollision()) {
 			this.lost = true;
 		}
-		occupiedSquares.add(this.x + (SQUARES + 10) * this.y);
+		this.drawToCanvas();
 	}
 }
 
@@ -83,57 +104,69 @@ sleep = function(ms) {
 }
 
 document.addEventListener('keydown', function(event) {
-	if (event.keyCode == 32) { // Space
-		main()
-	} else if(event.keyCode == 74) { // J
-        if(playerTwo) {
-			if(playerTwo.dir != "r"){
-				playerTwo.nextDir = "l";
+	switch (event.keyCode) {
+		case 32: // Space
+			main();
+			break;
+		case 74: // J
+			if (typeof playerTwo !== "undefined") {
+				if (playerTwo.dir != "r") {
+					playerTwo.nextDir = "l";
+				}
 			}
-		}
-    } else if(event.keyCode == 73) { // I
-        if(playerTwo) {
-			if(playerTwo.dir != "d"){
-				playerTwo.nextDir = "u";
+			break;
+		case 73: // I
+			if (typeof playerTwo !== "undefined") {
+				if (playerTwo.dir != "d") {
+					playerTwo.nextDir = "u";
+				}
 			}
-		}
-    } else if(event.keyCode == 76) { // L
-        if(playerTwo) {
-			if(playerTwo.dir != "l"){
-				playerTwo.nextDir = "r";
+			break;
+		case 76: // L
+			if (typeof playerTwo !== "undefined") {
+				if (playerTwo.dir != "l") {
+					playerTwo.nextDir = "r";
+				}
 			}
-		}
-    } else if(event.keyCode == 75) { // K
-        if(playerTwo) {
-			if(playerTwo.dir != "u"){
-				playerTwo.nextDir = "d";
+			break;
+		case 75: // K
+			if (typeof playerTwo !== "undefined") {
+				if (playerTwo.dir != "u") {
+					playerTwo.nextDir = "d";
+				}
 			}
-		}
-    } else if(event.keyCode == 87) { // W
-        if(playerOne) {
-			if(playerOne.dir != "d"){
-				playerOne.nextDir = "u";
+			break;
+		case 87: // W
+			if (typeof playerOne !== "undefined") {
+				if (playerOne.dir != "d") {
+					playerOne.nextDir = "u";
+				}
 			}
-		}
-	} else if(event.keyCode == 68) { // D
-		if(playerOne) {
-			if(playerOne.dir != "l"){
-				playerOne.nextDir = "r";
+			break;
+		case 68: // D
+			if (typeof playerOne !== "undefined") {
+				if (playerOne.dir != "l") {
+					playerOne.nextDir = "r";
+				}
 			}
-		}
-    } else if(event.keyCode == 83) { // S
-        if(playerOne) {
-			if(playerOne.dir != "u"){
-				playerOne.nextDir = "d";
+			break;
+		case 83: // S
+			if (typeof playerOne !== "undefined") {
+				if(playerOne.dir != "u") {
+					playerOne.nextDir = "d";
+				}
 			}
-		}
-    } else if(event.keyCode == 65) { // A
-        if(playerOne) {
-			if(playerOne.dir != "r"){
-				playerOne.nextDir = "l";
+			break;
+		case 65: // A
+			if (typeof playerOne !== "undefined") {
+				if(playerOne.dir != "r"){
+					playerOne.nextDir = "l";
+				}
 			}
-		}
-    }
+			break;
+		default:
+			break;
+	}
 });
 
 main = async function(){
@@ -142,7 +175,7 @@ main = async function(){
 		clearCanvas()
 		resultP.innerHTML = ""
 
-		occupiedSquares = new Set([])
+		squareOccupations = Array(SQUARES).fill().map(() => Array(SQUARES).fill(0));
 
 		players = [];
 		playerOne = new Tron("blue", 3, 3, "d");
@@ -154,11 +187,12 @@ main = async function(){
 		while(running) {
 			await sleep(SPEED);
 			for (i = 0; i < players.length; i++) {
+				players[i].preMove();
+			}
+			for (i = 0; i < players.length; i++) {
 				players[i].move();
 				if (players[i].lost) {
 					running = false;
-				} else {
-					players[i].drawToCanvas();
 				}
 			}
 		}
